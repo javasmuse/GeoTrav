f2d21615836da787cc029f0d752b43538a4675cb
javasmuse <candacelewis@mac.com>
2020-04-25 15:55:49 -0400

    uploading complete project

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB(); 
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Enable cors
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/v1/stores', require('./routes/stores'));
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
