3ec5e703aef137da3f5a5e2eab23cbf66a57ae55
javasmuse <candacelewis@mac.com>
2020-04-27 13:36:43 -0400

    updated port to 80, prevoius edits now original

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
-connectDB(); 
+connectDB();
 
 const app = express();
 
@@ -19,15 +19,15 @@ app.use(express.json());
 app.use(cors());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api/v1/stores', require('./routes/stores'));
+app.use("/api/v1/stores", require("./routes/stores"));
 
-
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Spoonbills running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
 );
