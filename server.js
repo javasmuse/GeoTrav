1c8b19fad8e2e2d0eea42e6a89c1b4aad09dc942
Candace <50122081+javasmuse@users.noreply.github.com>
2020-04-26 21:57:49 -0400

    Update server.js
    line 29 changed port to 80

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/v1/stores', require('./routes/stores'));
 
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () =>
   console.log(`Flamingos running in ${process.env.NODE_ENV} mode on port ${PORT}`)
