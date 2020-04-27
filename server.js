55ed32e744385aeba7a5ef97273d4dfb34bd8496
Candace <50122081+javasmuse@users.noreply.github.com>
2020-04-26 21:40:01 -0400

    Update server.js
    commented out line 11 and line 5

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
-connectDB(); 
+// connectDB(); 
 
 const app = express();
 
