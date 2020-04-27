0b91ad0b32b0d7080393f6d740907d7ebde3b6e1
Candace <50122081+javasmuse@users.noreply.github.com>
2020-04-26 21:47:24 -0400

    Update server.js
    changed line 29 from 5000 to 8080

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/v1/stores', require('./routes/stores'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
