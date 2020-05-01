4fc9c4d8ca5372a49d56d82b528d16be88a062a3
javasmuse <candacelewis@mac.com>
2020-05-01 12:24:30 -0400

    back to original working app

--- public/js/map.js
+++ public/js/map.js
@@ -4,55 +4,56 @@ const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
   zoom: 9,
-  center: [-76.157895, 36.907741]
+  center: [-76.157895, 36.907741],
 });
 
 // Fetch stores from API
 async function getStores() {
-    const res = await fetch('/api/v1/stores');
-    const data = await res.json();
+  const res = await fetch("/api/v1/stores");
+  const data = await res.json();
 
-    const stores = data.data.map(store => {
-        return {
-          type: "Feature",
-                geometry: {
-                  type: "Point",
-                  coordinates: [store.location.coordinates[0], store.location.coordinates[1]]
-                },
-                properties: {
-                    storeId: store.storeId,
-                    icon: 'shop'
-                }
-              }
-        });
-        loadMap(stores);
-    }
+  const stores = data.data.map((store) => {
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          store.location.coordinates[0],
+          store.location.coordinates[1],
+        ],
+      },
+      properties: {
+        storeId: store.storeId,
+        icon: "shop",
+      },
+    };
+  });
+  loadMap(stores);
+}
 
 // Load map with stores
 function loadMap(stores) {
-    map.on("load", function() {
-        map.addLayer({
-          id: "points",
-          type: "symbol",
-          source: {
-            type: "geojson",
-            data: {
-              type: "FeatureCollection",
-              features: stores,
-            }
-          },
-          layout: {
-            "icon-image": "{icon}-15",
-            "icon-size": 1.5,
-            'text-field': '{storeId}',
-            'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-            'text-offset': [0, 0.9],
-            'text-anchor': 'top'
-          }
-        });
-        }
-      );
-    };
+  map.on("load", function () {
+    map.addLayer({
+      id: "points",
+      type: "symbol",
+      source: {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: stores,
+        },
+      },
+      layout: {
+        "icon-image": "{icon}-15",
+        "icon-size": 1.5,
+        "text-field": "{storeId}",
+        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+        "text-offset": [0, 0.9],
+        "text-anchor": "top",
+      },
+    });
+  });
+}
 
-    getStores();
-                
+getStores();
